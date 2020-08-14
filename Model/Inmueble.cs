087ef95f6ca56b25e0c4c0825683884413dfc377
blankis27 <33439200+blankis27@users.noreply.github.com>
2020-08-14 20:26:38 -0300

    Add files via upload

---
+++ Model/Inmueble.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace MVVMInmobiliaria.ViewModels
+{
+    public class Inmueble
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, e);
+        }
+
+        private int _productId;
+        public int _ProductId { get { return _productId; } }
+
+        private string direccion;
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; OnPropertyChanged(new PropertyChangedEventArgs("Direccion"));
+                }
+        }
+
+        private string vendedor;
+        public string Vendedor
+        {
+            get { return vendedor; }
+            set { vendedor = value; OnPropertyChanged(new PropertyChangedEventArgs("Vendedor")); }
+        }
+
+        private string precio;
+        public string Precio
+        {
+            get { return precio; }
+            set { precio = value; OnPropertyChanged(new PropertyChangedEventArgs("Precio")); }
+        }
+
+        private string descripcion;
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; OnPropertyChanged(new PropertyChangedEventArgs("Descripcion")); }
+        }
+
+        private string categoria;
+        public string Categoria
+        {
+            get { return categoria; }
+            set { categoria = value; OnPropertyChanged(new PropertyChangedEventArgs("Categoria")); }
+        }
+
+        // STRING PARA MOSTRAR EN PANTALLA
+        public string InmuebleCompleto
+        {
+            get {return Direccion + "\t|\t" + Vendedor + "\t|\t" + Precio + "\t|\t" + Categoria; }
+        }
+
+        public Inmueble()
+        {
+        }
+
+        public Inmueble(int productId, string direccion, string vendedor,
+                       string precio, string descripcion, string categoria)
+        {
+            this._productId = productId;
+            Direccion = direccion;
+            Vendedor = vendedor;
+            Precio = precio;
+            Descripcion = descripcion;
+            Categoria = categoria;
+        }
+
+        public void CopyProduct(Inmueble inmueble)
+        {
+            this._productId = inmueble._ProductId;
+            this.Direccion = inmueble.Direccion;
+            this.Vendedor = inmueble.Vendedor;
+            this.Precio = inmueble.Precio;
+            this.Categoria = inmueble.Categoria;
+            this.Descripcion = inmueble.Descripcion;
+        }
+
+        public void ProductAdded2DB(SqlInmueble sqlInmueble)
+        {
+            this._productId = sqlInmueble.InmuebleId;
+        }
+
+
+    }
+
+    //Clase para comounicarse con SQL
+    public class SqlInmueble
+    {
+        public int InmuebleId { get; set; }
+        public string Direccion {get; set;}
+        public string Vendedor {get; set;}
+        public decimal Precio {get; set;}
+        public string Descripcion {get; set;}
+        public string Categoria {get; set;}
+
+        public SqlInmueble() { }
+
+        public SqlInmueble(int inmuebleId, string direccion, string vendedor,
+                       decimal precio, string descripcion, string categoria)
+        {
+            InmuebleId = inmuebleId;
+            Direccion = direccion;
+            Vendedor = vendedor;
+            Precio = precio;
+            Descripcion = descripcion;
+            Categoria = categoria;
+        }
+
+        public SqlInmueble(Inmueble inmueble)
+        {
+            InmuebleId = inmueble._ProductId;
+            Direccion = inmueble.Direccion;
+            Vendedor = inmueble.Vendedor;
+            Precio = Convert.ToDecimal(inmueble.Precio);
+            Descripcion = inmueble.Descripcion;
+            Categoria = inmueble.Categoria;
+        }
+
+        public Inmueble SqlProduct2Product()
+        {
+            string precio = Precio.ToString();
+            return new Inmueble(InmuebleId, Direccion, Vendedor, precio, Descripcion, Categoria);
+        }
+    }
+
+}
---
+++ Model/StoreDB.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.ObjectModel;
+using MVVMInmobiliaria;
+using MVVMInmobiliaria.ViewModels;
+
+namespace MVVMInmobiliaria.Model
+{
+    public class StoreDB
+    {
+        public bool hasError = false;
+        public string errorMessage;
+        
+        public MyObservableCollection<Inmueble> GetInmuebles()
+        {
+            hasError = false;
+            MyObservableCollection<Inmueble> inmueble = new MyObservableCollection<Inmueble>();
+            try
+            {
+                LinqDataContext dc = new LinqDataContext();
+                var query = from q in dc.LinqProducts
+                    select new SqlInmueble{
+                        InmuebleId = q.ProductID, Direccion = q.ModelNumber,
+                        Vendedor=q.ModelName, Precio = (decimal)q.UnitCost,
+                        Descripcion = q.Description, Categoria = q.LinqCategory.CategoryName
+                    };
+                foreach (SqlInmueble sp in query)
+                    inmueble.Add(sp.SqlProduct2Product());
+            }
+            catch(Exception ex)
+            {
+                errorMessage = "GetInmuebles() error, " + ex.Message;
+                hasError = true;
+            }
+            return inmueble;
+        }
+
+        public bool UpdateInmueble(Inmueble mostrarInmueble)
+        {
+            try
+            {
+                SqlInmueble inmueble = new SqlInmueble(mostrarInmueble);
+                LinqDataContext dc = new LinqDataContext();
+                dc.UpdateInmueble(inmueble.InmuebleId, inmueble.Categoria, inmueble.Direccion, inmueble.Vendedor, inmueble.Precio, inmueble.Descripcion);
+            }
+            catch (Exception ex)
+            {
+                errorMessage = "Update error, " + ex.Message;
+                hasError = true;
+            }
+            return (!hasError);
+        }
+
+        public bool DeleteInmueble(int inmuebleId)
+        {
+            hasError = false;
+            try
+            {
+                LinqDataContext dc = new LinqDataContext();
+                dc.DeleteInmueble(inmuebleId);
+            }
+            catch (Exception ex)
+            {
+                errorMessage = "Delete error, " + ex.Message;
+                hasError = true;
+            }
+            return !hasError;
+        }
+
+        public bool AddInmueble(Inmueble mostrarInmueble)
+        {
+            hasError = false;
+            try
+            {
+                SqlInmueble inmueble = new SqlInmueble(mostrarInmueble);
+                LinqDataContext dc = new LinqDataContext();
+                int? nuevoInmuebleId = 0;
+                dc.AddInmueble(inmueble.Categoria, inmueble.Direccion, inmueble.Vendedor, inmueble.Precio, inmueble.Descripcion, ref nuevoInmuebleId);
+                inmueble.InmuebleId = (int)nuevoInmuebleId;
+                mostrarInmueble.ProductAdded2DB(inmueble);
+            }
+            catch (Exception ex)
+            {
+                errorMessage = "Add error, " + ex.Message;
+                hasError = true;
+            }
+            return !hasError;
+        }
+    }
+}
